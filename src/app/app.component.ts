f43f980bd5c23cdd0231171daffd2d6985a669e0
Olivier Leblond <oleblond@psi-it.com>
2024-12-08 09:34:07 -0500

    With meta tags

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { ChangeDetectionStrategy, Component, isDevMode } from '@angular/core';
+import { ChangeDetectionStrategy, Component, isDevMode, OnInit } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { environment } from 'src/environments/environment';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   standalone: true,
@@ -13,18 +14,27 @@ import { environment } from 'src/environments/environment';
   imports: [CommonModule, MatToolbarModule, MatIconModule, RouterOutlet],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   isDevMode: boolean = isDevMode();
   apiUrl = environment.apiUrl;
   showWarning = false;
   //isOrderForm: boolean = false;
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private title: Title, private meta: Meta) {
     // Check that UAT is not using the prod backend
     if (this.isDevMode && this.apiUrl.includes('prod')) {
       this.showWarning = true;
       alert('WARNING: Angular is in Dev mode but connected to production !!!');
     }
+  }
+
+  ngOnInit(): void {
+    this.title.setTitle('Pacific Fresh Fish Company App');
+    this.meta.addTags([
+      { name: 'description', content: 'Pacific Fresh Fish Company application created by lb-advisors' },
+      { name: 'keywords', content: 'PFF, lb-advisors, allintel' },
+      { name: 'robots', content: 'index, follow' },
+    ]);
   }
 
   //ngOnInit(): void {
