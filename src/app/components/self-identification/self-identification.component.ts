8d7dc12f4577f27ed722499563dc299d72440d4c
advarjabedian <advarjabedian@gmail.com>
2025-02-05 10:59:26 -0800

    self identification form

--- src/app/components/self-identification/self-identification.component.ts
+++ src/app/components/self-identification/self-identification.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-self-identification',
   standalone: true,
-  imports: [],
+  selector: 'app-self-identification',
   templateUrl: './self-identification.component.html',
-  styleUrl: './self-identification.component.css'
+  styleUrls: ['./self-identification.component.scss'],
+  imports: [CommonModule, ReactiveFormsModule]
 })
-export class SelfIdentificationComponent {
+export class SelfIdentificationComponent implements OnInit {
+  identificationForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.identificationForm = this.fb.group({
+      name: ['', Validators.required],
+      positionTitle: ['', Validators.required],
+      date: ['', Validators.required],
+      legalSex: ['', Validators.required],
+      raceEthnicity: ['', Validators.required]
+    });
+  }
+
+  ngOnInit(): void {
+    // Set today's date as default
+    const today = new Date().toISOString().split('T')[0];
+    this.identificationForm.patchValue({
+      date: today
+    });
+  }
+
+  onSubmit(): void {
+    if (this.identificationForm.valid) {
+      console.log(this.identificationForm.value);
+      // Handle form submission
+    }
+  }
 }
