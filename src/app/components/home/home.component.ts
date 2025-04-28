e9042a51413bb05238e849f6531935ad6db58ee5
advarjabedian <advarjabedian@gmail.com>
2025-04-28 01:28:29 -0700

    updating nav

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -14,8 +14,8 @@ import { AuthService } from 'src/app/services/auth.service';
 export class HomeComponent implements OnInit {
   username!: string | null;
   isHROpen: boolean = false;
-  isSalesOpen: boolean = false;
-  isOtherOpen: boolean = false; // New property for Other dropdown
+  isOtherOpen: boolean = false;
+  // Removed isSalesOpen property since we no longer need it
 
   constructor(
     @Inject(Router) private router: Router,
@@ -30,7 +30,6 @@ export class HomeComponent implements OnInit {
     this.router.navigate([`/${page}`]);
     // Close all dropdowns after navigation
     this.isHROpen = false;
-    this.isSalesOpen = false;
     this.isOtherOpen = false;
   }
 
@@ -38,27 +37,18 @@ export class HomeComponent implements OnInit {
     this.isHROpen = !this.isHROpen;
     // Close other dropdowns when opening HR
     if (this.isHROpen) {
-      this.isSalesOpen = false;
       this.isOtherOpen = false;
     }
   }
 
-  toggleSales(): void {
-    this.isSalesOpen = !this.isSalesOpen;
-    // Close other dropdowns when opening Sales
-    if (this.isSalesOpen) {
-      this.isHROpen = false;
-      this.isOtherOpen = false;
-    }
-  }
+  // Removed toggleSales() method since we no longer need it
 
-  // New method for Other dropdown
+  // Method for Other dropdown
   toggleOther(): void {
     this.isOtherOpen = !this.isOtherOpen;
     // Close other dropdowns when opening Other
     if (this.isOtherOpen) {
       this.isHROpen = false;
-      this.isSalesOpen = false;
     }
   }
 
